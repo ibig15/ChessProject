aa34032230e3663788bace1e2a6d7cd8f70e2dfb
ibig15 <ib.irinabig@gmail.com>
2016-05-26 15:23:56 +0100

    Update Pawn class

--- ChessProject-Java/src/com/logicnow/hiring/Pawn.java
+++ ChessProject-Java/src/com/logicnow/hiring/Pawn.java
@@ -1,59 +1,20 @@
 package com.logicnow.hiring;
 
-public class Pawn {
+import java.lang.Math;
 
-    private ChessBoard chessBoard;
-    private int xCoordinate;
-    private int yCoordinate;
-    private PieceColor pieceColor;
+public class Pawn extends Piece {
 
     public Pawn(PieceColor pieceColor) {
-        this.pieceColor = pieceColor;
+    	super(pieceColor, PieceType.PAWN);
     }
-
-    public ChessBoard getChesssBoard() {
-        return chessBoard;
-    }
-
-    public void setChessBoard(ChessBoard chessBoard) {
-        this.chessBoard = chessBoard;
-    }
-
-    public int getXCoordinate() {
-        return xCoordinate;
-    }
-
-    public void setXCoordinate(int value) {
-        this.xCoordinate = value;
-    }
-
-    public int getYCoordinate() {
-        return yCoordinate;
-    }
-
-    public void setYCoordinate(int value) {
-        this.yCoordinate = value;
-    }
-
-    public PieceColor getPieceColor() {
-        return this.pieceColor;
-    }
-
-    private void setPieceColor(PieceColor value) {
-        pieceColor = value;
-    }
-
-    public void Move(MovementType movementType, int newX, int newY) {
-        throw new UnsupportedOperationException("Need to implement Pawn.Move()") ;
-    }
-
-    @Override
-    public String toString() {
-        return CurrentPositionAsString();
-    }
-
-    protected String CurrentPositionAsString() {
-        String eol = System.lineSeparator();
-        return String.format("Current X: {1}{0}Current Y: {2}{0}Piece Color: {3}", eol, xCoordinate, yCoordinate, pieceColor);
+    
+    public boolean IsLegalMove(int oldX, int oldY, int newX, int newY) {
+    	if ( (this.pieceColor == PieceColor.WHITE && newX - oldX == 1 && Math.abs(oldY - newY) <= 1) ||
+    		 (this.pieceColor == PieceColor.BLACK && oldX - newX == 1 && Math.abs(oldY - newY) <= 1)){
+    		return true;
+    	}
+    	else{
+   			return false;
+   		}
     }
 }
